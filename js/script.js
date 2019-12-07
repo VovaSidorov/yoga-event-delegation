bf0fb11523852916df722141aec89a8e5bb501c1
Vladimir <VovaCivic@gmail.com>
2019-12-08 00:03:32 +0200

    add calculator

--- js/script.js
+++ js/script.js
@@ -198,8 +198,8 @@ window.addEventListener("DOMContentLoaded", function () {
     function plusSlides(n){
         showSlides(slideIndex+=n);
     }
-    function currentslide(n){
 
+    function currentslide(n){
         showSlides(slideIndex=n);
     }
 
@@ -217,6 +217,49 @@ window.addEventListener("DOMContentLoaded", function () {
             }
         }
     });
+
+//  Calc
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum =0,
+        total =0;
+
+        totalValue.innerHTML = 0;
+
+        persons.addEventListener('input', function () {
+            personsSum = +this.value;
+            total = (daysSum + personsSum)*4000;
+            if (restDays.value=='' || persons.value==''){
+                totalValue.innerHTML=0;
+            }
+            else {
+                totalValue.innerHTML=total;
+            }
+        });
+
+        restDays.addEventListener('input', function () {
+            daysSum = +this.value;
+            total = (daysSum + personsSum)*4000;
+            if (persons.value=='' || restDays.value==''){
+                totalValue.innerHTML=0;
+            }
+            else {
+                totalValue.innerHTML=total;
+            }
+        });
+
+        place.addEventListener('change',function () {
+            if (restDays.value=='' || persons.value==''){
+                totalValue.innerHTML=0;
+            }else{
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+        });
 
 });
 
