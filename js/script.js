59d80822a4d6c6f3279d6f5b8f3b2c53e8a1c8c9
Vladimir <VovaCivic@gmail.com>
2019-12-07 21:29:13 +0200

    sending data in json format

--- js/script.js
+++ js/script.js
@@ -139,11 +139,16 @@ window.addEventListener("DOMContentLoaded", function () {
 
         let request = new XMLHttpRequest();
         request.open('POST','server.php');
-        request.setRequestHeader('Content-type','application/x-wwww-form-unlencoded');
+        request.setRequestHeader('Content-type','application/json; charset=utf-8');
 
         let formData = new FormData(form);
-        request.send(formData);
+        let obj = {};
+        formData.forEach(function (value,key) {
+              obj[key]=value;
+        });
+        let json = JSON.stringify(obj);
 
+        request.send(json);
 
         request.addEventListener('readystatechange', function () {
             if (request.readyState < 4){
