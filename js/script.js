882a3b96453398f6e97d165011427933eda7d932
Vladimir <VovaCivic@gmail.com>
2019-12-07 22:58:20 +0200

    added slider

--- js/script.js
+++ js/script.js
@@ -127,6 +127,9 @@ window.addEventListener("DOMContentLoaded", function () {
         failure: 'Что то пошло не так...'
     };
 
+
+    //send form
+
     let form = document.querySelector(".main-form"),
         input = form.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
@@ -165,7 +168,59 @@ window.addEventListener("DOMContentLoaded", function () {
             input[i].value = '';
         }
     });
+
+
+
+//    slider
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+
+        if (n>slides.length){
+            slideIndex=1;
+        }
+        if (n<1){
+            slideIndex = slides.length;
+        }
+        slides.forEach((item)=>item.style.display='none');
+        dots.forEach((item)=>item.classList.remove('dot-active'));
+        slides[slideIndex-1].style.display='block';
+        dots[slideIndex-1].classList.add("dot-active");
+    }
+
+    function plusSlides(n){
+        showSlides(slideIndex+=n);
+    }
+    function currentslide(n){
+
+        showSlides(slideIndex=n);
+    }
+
+    prev.addEventListener('click',function () {
+        plusSlides(-1);
+    });
+    next.addEventListener('click',function () {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click',function (event) {
+        for (let i=0;i<dots.length+1;i++){
+            if(event.target.classList.contains('dot')&& event.target == dots[i-1]){
+                currentslide(i);
+            }
+        }
+    });
+
 });
+
+
 
 // class Options {
 //     constructor(height, width, bg, fontSize, textAlign,text,elementClass,tag){
